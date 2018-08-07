e621974954898a51dad66f494f31db483c4a1e53
Cristian Moraru <cristian.moraru@live.com>
2018-08-07 10:13:36 +0200

    Added repository wrapper implementation

---
+++ MongoDB/Repository/RepositoryWrapper.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Database.Models;
+
+namespace MongoDB.Repository
+{
+    public class RepositoryWrapper : IRepositoryWrapper
+    {
+        public IUserRepository<User> Users { get; }
+        public IPostRepository<Post> Posts { get; }
+
+        public RepositoryWrapper(IUserRepository<User> users, IPostRepository<Post> posts)
+        {
+            Users = users;
+            Posts = posts;
+        }
+    }
+}
