735e6e9ba2b89c53c222a966c73cb98e557b0c6f
Cristian Moraru <cristian.moraru@live.com>
2018-08-07 10:13:24 +0200

    Added repository wrapper interface

---
+++ MongoDB/Repository/IRepositoryWrapper.cs
@@ -0,0 +1,10 @@
+﻿using MongoDB.Database.Models;
+
+namespace MongoDB.Repository
+{
+    public interface IRepositoryWrapper
+    {
+        IUserRepository<User> Users { get; }
+        IPostRepository<Post> Posts { get; }
+    }
+}
