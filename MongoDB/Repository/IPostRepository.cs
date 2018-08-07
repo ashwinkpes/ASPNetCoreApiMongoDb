dd23732e4a6f18794f0b50e8e8f7d6ca00bc2f78
Cristian Moraru <cristian.moraru@live.com>
2018-08-07 11:25:15 +0200

    Added type restrictions to IPostRepository and IUserRepository

--- MongoDB/Repository/IPostRepository.cs
+++ MongoDB/Repository/IPostRepository.cs
@@ -1,6 +1,8 @@
-﻿namespace MongoDB.Repository
+﻿using MongoDB.Database.Models;
+
+namespace MongoDB.Repository
 {
-    public interface IPostRepository<T> : IRepository<T> where T : class
+    public interface IPostRepository<T> : IRepository<T> where T : class, IPost
     {
 
     }
--- MongoDB/Repository/IUserRepository.cs
+++ MongoDB/Repository/IUserRepository.cs
@@ -5,10 +5,8 @@ using System.Threading.Tasks;
 
 namespace MongoDB.Repository
 {
-    public interface IUserRepository<T> : IRepository<T> where T : class
+    public interface IUserRepository<T> : IRepository<T> where T : class, IUser
     {
-        Task<Token> SignInAsync(T model);
-        Task<Token> SignUpAsync(T model);
         Task AddPostAsync(Guid id, Post post);
     }
 }
