8b85cad5f920f7ffe6f4b3d3e9f3b54d1ecd6110
Cristian Moraru <cristian.moraru@live.com>
2018-08-07 10:12:28 +0200

    Added account manager to handle signin and signup instead of the user repository

---
+++ MongoDB/Manager/AccountManager.cs
@@ -0,0 +1,50 @@
+﻿using MongoDB.Database;
+using MongoDB.Database.Models;
+using MongoDB.Driver;
+
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MongoDB.Manager
+{
+    public class AccountManager : IAccountManager<User>
+    {
+        private readonly IMongoContext _context;
+
+        public AccountManager(IMongoContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Token> SignInAsync(User model)
+        {
+            // Just for demostration purposes
+            User user = await _context
+                .Users
+                .Find(x => x.Email == model.Email && x.Password == model.Password)
+                .FirstOrDefaultAsync();
+
+            if (user is null)
+                return null;
+
+            byte[] secureHash = Encoding.UTF8.GetBytes("SuperSecureHash");
+            return new Token(secureHash, DateTime.Now.AddMinutes(10));
+        }
+
+        public async Task<Token> SignUpAsync(User model)
+        {
+            User user = await _context
+                .Users
+                .Find(x => x.Email == model.Email)
+                .FirstOrDefaultAsync();
+
+            if (user != null)
+                throw new InvalidOperationException("User already exists");
+
+            await _context.Users.InsertOneAsync(model);
+            byte[] secureHash = Encoding.UTF8.GetBytes("SuperSecureHash");
+            return new Token(secureHash, DateTime.Now.AddMinutes(10));
+        }
+    }
+}
---
+++ MongoDB/Manager/IAccountManager.cs
@@ -0,0 +1,12 @@
+﻿using MongoDB.Database.Models;
+
+using System.Threading.Tasks;
+
+namespace MongoDB.Manager
+{
+    public interface IAccountManager<T> where T : class, IUser
+    {
+        Task<Token> SignInAsync(T model);
+        Task<Token> SignUpAsync(T model);
+    }
+}
