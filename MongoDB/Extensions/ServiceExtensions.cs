93ec72e8466e4c846f528b1477eb0bb0af27d4ca
Cristian Moraru <cristian.moraru@live.com>
2018-08-07 10:10:32 +0200

    Added service extension methods.

---
+++ MongoDB/Extensions/ServiceExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+using MongoDB.Database;
+using MongoDB.Database.Models;
+using MongoDB.Manager;
+using MongoDB.Repository;
+
+namespace MongoDB.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static IServiceCollection AddRepository(this IServiceCollection services)
+        {
+            services.AddScoped<IMongoContext, MongoContext>();
+            services.AddScoped<IUserRepository<User>, UserRepository>();
+            services.AddScoped<IPostRepository<Post>, PostRepository>();
+            services.AddScoped<IRepositoryWrapper, RepositoryWrapper>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddManagers(this IServiceCollection services)
+        {
+            services.AddScoped<IAccountManager<User>, AccountManager>(); 
+
+            return services;
+        }
+    }
+}
